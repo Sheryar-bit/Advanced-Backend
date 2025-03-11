33a3fd682895c99e271f6f612015b406ec379f67
Sheryar <alisharyar93@gmail.com>
2025-03-11 23:53:43 +0500

    CorsConfigure(Best-Practice)

--- Advance_Express/corsConfig.js
+++ Advance_Express/corsConfig.js
@@ -9,11 +9,27 @@ const configcors = function() {
                 'httpx://MycostumDomain', //production custom domain
             ]
             // if the origin is not in the allowed list :
-            if(!origin || allowedorigin.indexOf(origin !== -1)){
+            if(!origin || allowedorigin.indexOf(origin) !== -1){
                 return callback(null, true) //giving permission
             } else {
                 return callback(new Error('Not allowed by CORS'))
             }
-        }
+        },
+
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization',
+            'Accept'
+        ],
+        //expose the response to the client
+        exposeHeaders: ['Content-Range', 'ETag'],
+
+        //Credentials:
+        credentials: true, //This will enable support to cookies
+        //OptionSuccessstatusCode:
+        optionsSuccessStatus: 204 //If you want to return a 204 status code instead
     })
 }
+
+module.exports = { configcors };
--- Advance_Express/server.js
+++ Advance_Express/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 require('dotenv').config();
+const {configcors} = require('./corsConfig')
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(configcors());
+
+app.get('/', (req, res) => {
+    res.send('Practice')
+})
 
 app.listen(PORT, function() {
     console.log(`Server is running on ${PORT}`);
