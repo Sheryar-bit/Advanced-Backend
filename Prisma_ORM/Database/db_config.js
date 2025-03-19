d390c09a982790d3f9a0cbd6782b73d4a22e460e
Sheryar <alisharyar93@gmail.com>
2025-03-19 20:50:17 +0500

    Removed old folder: prisma_ORM

--- Prisma_ORM/Database/db_config.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient({
-    log:["query"]
-});
-
-//Now I can start sending queries via the generated Prisma Client API
-
-module.exports =  prisma ;
